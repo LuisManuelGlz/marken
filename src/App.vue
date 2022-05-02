56731e1130c046d808532e93ff930ffd900598dd
Luis Manuel González <luismanuelglz.dev@gmail.com>
2022-05-01 20:31:27 -0700

    Add input and viewer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navigation">
+      <img class="vueLogo" alt="Vue logo" src="./assets/logo.png" />
+    </nav>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.navigation {
+  text-align: center;
+  padding: 10px 40px;
+  background: #f0f0f0;
+}
+
+.vueLogo {
+  width: 40px;
 }
 </style>
